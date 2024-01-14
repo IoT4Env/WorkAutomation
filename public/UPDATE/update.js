a99108839469009ab7fad8710880f5613ce054e2
BioTech4Env <biotech.4env@gmail.com>
2024-01-14 12:54:09 +0100

    Solved Issue #6

--- public/UPDATE/update.js
+++ public/UPDATE/update.js
@@ -1,5 +1,8 @@
-const updateRow = document.querySelector('#update-row')
+const inputs = document.getElementsByTagName('input');
+const regex = /[^a-z0-9]/gi
 
-updateRow.addEventListener('click', _ => {
-
-})
+for (let i = 0; i < inputs.length - 1; i++) {
+    inputs[i].addEventListener('keyup', _ => {
+        inputs[i].value = inputs[i].value.replace(regex, "")
+    })
+}
