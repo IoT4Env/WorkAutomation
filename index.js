6db00d51141e80a527dded64c91e5de262104b8e
BioTech4Env <biotech.4env@gmail.com>
2024-03-30 16:04:43 +0100

    Better organized code

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-import dotEnv from 'dotenv'
 import express from 'express';
 import helmet from 'helmet';
-import SQLite3 from 'sqlite3';
-import path, { join } from 'path';
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
 
 import crud from './Routes/CRUD.js';
 import image from './Views/imageView.js';
 import handleError from './Views/handleError.js';
 import fileUpload from './Routes/FileUpload.js';
 
-dotEnv.config()
+import Resources from './Resources/resources.js'
+import Config from './Resources/config.js'
 
-const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME;
-const SERVER_PORT = process.env.SERVER_PORT
-const url = `http://${SERVER_HOSTNAME}:${SERVER_PORT}`
+const resources = new Resources();
+const config = new Config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const ddl = resources.HtmlTemplates.DDL;
+
+const SERVER_PORT = config.SERVER_PORT
+const SERVER_HOSTNAME = config.SERVER_HOSTNAME
+const localhost = config.URL;
+
+const modelsDb = resources.ModelsDb;
 
 const app = express();
-
 app.use(helmet())
 app.use(express.json());
-
-let htmlDropDownPopulation = readFileSync(__dirname + '/public/MainPage/Index.html', 'utf-8')
 
 app.use('/CRUD', crud)
 app.use('/FileUpload', fileUpload)
 app.use('/Images', image)
 app.use('/handleError', handleError)
 
-const modelsDb = new SQLite3.Database(join(__dirname + '/Databases/models.db'))
 
 app.listen(SERVER_PORT, SERVER_HOSTNAME, _ => {
-    console.log(`Server started on ${url}`)
+    console.log(`Server started on ${localhost}`)
 })
 
+//Gets main page
 app.get('', (req, res) => {
     modelsDb
     .serialize(_ => {
@@ -59,7 +56,7 @@ app.get('', (req, res) => {
             })
             app.use(express.static('./public/MainPage'))
             res.status(200)
-                .send(htmlDropDownPopulation.replace('{{%Names%}}', populatedNames.join(',')))
+                .send(ddl.replace('{{%Names%}}', populatedNames.join(',')))
         })
     })
 })
