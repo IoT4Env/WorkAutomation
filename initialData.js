b32194b279a66873821d5b75c5682b55a3b654f5
BioTech4Env <biotech.4env@gmail.com>
2024-04-06 21:03:57 +0200

    Code adjustments

--- initialData.js
+++ initialData.js
@@ -13,15 +13,10 @@ decompress('contentFile.ods', {
      * file-like representation of the actual file
      */
     const xmlData = files[0].data.toString().split('><').join('>\n<').split('\n')
-    console.time('xml to csv')
     const csvData = xmlToCsv(xmlData)
-    console.timeEnd('xml to csv')
-    console.time('csv to sql')
     const sqlQuery = csvToSql(csvData)
-    console.timeEnd('csv to sql')
-    console.log(csvData);
 
-    //fs.writeFileSync(path.join(resources.__dirname, '/SqlQueries/insert.sql'), sqlQuery)
+    fs.writeFileSync(path.join(resources.__dirname, '/SqlQueries/insertByJs.sql'), sqlQuery)
 });
 
 /**
@@ -35,7 +30,6 @@ function xmlToCsv(xml) {
 
     // The end of a row in the ods file needs corresponds to 2 xml tags to be closed (cell and row)
     // AND 2 xml tags to be opened (cell and row)
-    // if this variable exeeds the sum of 2 + 2, a new row is read
     let rowDelimitator = 0
     xml.forEach(data => {
         let index = data.indexOf('>')
@@ -49,15 +43,15 @@ function xmlToCsv(xml) {
             while (data[++index] != '<') {
                 cellContent += data[index]
             }
-            if(/[^0-9]/.test(cellContent)){
+            if (/[^0-9]/.test(cellContent)) {
                 cellContent = `"${cellContent}"`
             }
-            odsRow.push(cellContent)
+            odsRow.push(cellContent.toLowerCase())
             rowDelimitator = 0;
         }
 
+        // if this variable exeeds the sum of 2 + 2, a new row is read
         if (rowDelimitator > 3 && odsRow.length > 0) {
-            //new row
             fullOdsContent.push(odsRow.join(';'))
             odsRow = []
             rowDelimitator = 0;
@@ -67,13 +61,11 @@ function xmlToCsv(xml) {
 }
 
 function csvToSql(csv) {
-    let sqlQuery = `INSERT INTO Models (${csv[0]
-        .replace(/;/g, ',')
-        .replace(/"/g, '')}
-        ) VALUES %VALUES%;`
     let sqlValues = []
     for (let i = 1; i < csv.length; i++) {
         sqlValues.push(`(${csv[i].replace(/;/g, ',')})`)
     }
-    return sqlQuery.replace('%VALUES%', sqlValues.join(',\n'))
+    return `INSERT INTO Models (${csv[0]
+        .replace(/;/g, ',')
+        .replace(/"/g, '')}) VALUES \n${sqlValues.join(',\n')};`
 }
