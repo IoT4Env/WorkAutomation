e988a0ce1a63ecfb59ba0bd3c69c8580dd44f714
BioTech4Env <biotech.4env@gmail.com>
2024-04-08 14:52:26 +0200

    Changed names on public folder

--- Resources/resources.js
+++ Resources/resources.js
@@ -14,10 +14,10 @@ export default class Resources {
     HtmlTemplates = {
         DDL: readFileSync(join(this.__dirname, '../public/MainPage/Index.html'), 'utf-8'),
         Content: readFileSync(join(this.__dirname, '../public/ContentBody.html'), 'utf-8'),
-        Get: readFileSync(join(this.__dirname, '../public/GETS/GETS.html'), 'utf-8'),
-        Post: readFileSync(join(this.__dirname, '../public/POST/POST.html'), 'utf-8'),
-        Update: readFileSync(join(this.__dirname, '../public/UPDATE/UPDATE.html'), 'utf-8'),
-        Delete: readFileSync(join(this.__dirname, '../public/DELETE/DELETE.html'), 'utf-8')
+        Get: readFileSync(join(this.__dirname, '../public/GETS/gets.html'), 'utf-8'),
+        Post: readFileSync(join(this.__dirname, '../public/POST/post.html'), 'utf-8'),
+        Update: readFileSync(join(this.__dirname, '../public/UPDATE/update.html'), 'utf-8'),
+        Delete: readFileSync(join(this.__dirname, '../public/DELETE/delete.html'), 'utf-8')
     }
     ReturnBackButton = `<button><a href="/">BACK</a></button>`
 
