c5fb8b6ad3cd26e0fbaad55c973f0cae163d8988
BioTech4Env <biotech.4env@gmail.com>
2024-04-21 14:47:26 +0200

    Managed to update selections for different filters

--- Views/fetchResources.js
+++ Views/fetchResources.js
@@ -1,15 +1,42 @@
 import express from 'express'
 
 import Resources from '../Resources/resources.js'
-
+import Apis from '../Resources/apis.js'
 
 const resources = new Resources();
+const apis = new Apis();
+
+const modelsDb = resources.ModelsDb
 
 const frontBack = express()
 
 frontBack.get('/columns', (req,res) =>{
     res.status(200).send(resources.ColumnsName);
     return
+})
+
+frontBack.get('/filters/:filter', (req,res) =>{
+    const selectedFilter = req.params.filter
+    modelsDb.serialize(_ => {
+        modelsDb.all(`SELECT ROWID, ${selectedFilter} FROM Models`, (err, rows) => {
+            if (err) {
+                console.log(err);
+                const errorObj = {
+                    "Code": 1,
+                    "Body": err
+                }
+                res.redirect(`/handleError/${JSON.stringify(errorObj)}`)
+                return;
+            }
+            let filters = []
+            rows.map(json => {
+                if (!filters.includes(`<option>${json[selectedFilter]}</option>`))
+                    filters.push(`<option>${json[selectedFilter]}</option>`)
+            })
+
+            return res.status(200).send(filters)
+        })
+    })
 })
 
 export default frontBack;
--- index.js
+++ index.js
@@ -13,7 +13,8 @@ import Config from './Resources/config.js'
 const resources = new Resources();
 const config = new Config();
 
-const ddl = resources.HtmlTemplates.DDL;
+const index = resources.HtmlTemplates.Index;
+let lastFilter = ''
 
 const SERVER_PORT = config.SERVER_PORT
 const SERVER_HOSTNAME = config.SERVER_HOSTNAME
@@ -24,6 +25,10 @@ const modelsDb = resources.ModelsDb;
 const app = express();
 app.use(helmet())
 app.use(express.json());
+app.use(express.static('public'));
+
+app.set('views', resources.HtmlPaths.Index);
+app.set('view engine', 'ejs');
 
 app.use('/CRUD', crud)
 app.use('/FileUpload', fileUpload)
@@ -38,29 +43,33 @@ app.listen(SERVER_PORT, SERVER_HOSTNAME, _ => {
 
 //Gets main page
 app.get('', (req, res) => {
+    lastFilter = Object.keys(req.query).length === 0 ? 'name' : req.query['filter'];
     modelsDb.serialize(_ => {
-        modelsDb.all('SELECT ROWID, name FROM Models', (err, rows) => {
+        modelsDb.all(`SELECT ROWID, ${lastFilter} FROM Models`, (err, rows) => {
             if (err) {
+                console.log(err);
                 const errorObj = {
                     "Code": 1,
                     "Body": err
                 }
-                res.redirect(`/handleError/:${JSON.stringify(errorObj)}`)
+                res.redirect(`/handleError/${JSON.stringify(errorObj)}`)
                 return;
             }
-            let jsonTemplate = JSON.parse(JSON.stringify(rows))
+            //let jsonTemplate = JSON.parse(JSON.stringify(rows))
             let names = []
-            jsonTemplate.map(json => {
-                if (!names.includes(`<option>${json.name}</option>`))
-                    names.push(`<option>${json.name}</option>`)
+            //console.log(jsonTemplate);
+            rows.map(json => {
+                if (!names.includes(`<option>${json[lastFilter]}</option>`))
+                    names.push(`<option>${json[lastFilter]}</option>`)
             })
             let filters = []
             resources.ColumnsName.forEach(column => {
                 filters.push(`<option>${column}</option>`)
             })
-            app.use(express.static('./public/MainPage'))
-            res.status(200)
-                .send(ddl.replace('{{%FIELDS%}}', names.join(','))
+            app.use(express.static(resources.HtmlPaths.Index));
+            return res.status(200).send(
+                resources.HtmlTemplates.Index
+                    .replace('{{%FIELDS%}}', names)
                     .replace('{{%FILTERS%}}', filters))
         })
     })
