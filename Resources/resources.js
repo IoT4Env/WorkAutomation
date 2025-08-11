9a231d99ccd345a4e761296ca34c91206b3f6cfd
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 12:24:34 +0200

    Managed to save configuration setting on json file

--- Resources/resources.js
+++ Resources/resources.js
@@ -1,5 +1,5 @@
 import SQLite3 from 'sqlite3';
-import { readFileSync } from 'fs'
+import { readFileSync, writeFileSync } from 'fs'
 import path, { join } from 'path'
 import { fileURLToPath } from 'url';
 
@@ -14,6 +14,9 @@ export default class Resources {
     HtmlTemplates = {
         Index: readFileSync(join(this.__dirname, '../public/MainPage/index.html'), 'utf-8'),
         HamburgerMenu: readFileSync(join(this.__dirname, '../public/HamburgerMenu/hamburgerMenu.html'), 'utf-8'),
+        Menu:{
+            Settings: readFileSync(join(this.__dirname, '../public/Menu/Settings/settings.html'), 'utf-8'),
+        },
         Content: readFileSync(join(this.__dirname, '../public/CttBody/ContentBody.html'), 'utf-8'),
         ConfirmDeletion: readFileSync(join(this.__dirname, '../public/ConfirmDeletion/ConfirmDeletion.html'), 'utf-8'),
         Get: readFileSync(join(this.__dirname, '../public/GETS/gets.html'), 'utf-8'),
@@ -23,6 +26,9 @@ export default class Resources {
         Delete: readFileSync(join(this.__dirname, '../public/DELETE/delete.html'), 'utf-8')
     }
 
+    MenuConfigPath = join(this.__dirname, 'menuConfig.json')
+    MenuConfigCtt = JSON.parse(readFileSync(this.MenuConfigPath, 'utf-8'))
+    
     ReturnBackButton = `<button><a href="/">BACK</a></button>`
 
     //Names as params, so must be capitalized
@@ -37,5 +43,9 @@ export default class Resources {
         GetByField: (field) =>{
             return `SELECT ROWID, ${field} FROM Models`
         }
+    }
+
+    UpdateMenuConfig = (data) => {
+        writeFileSync(this.MenuConfigPath, JSON.stringify(data))
     }
 }
--- Routes/menu.js
+++ Routes/menu.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import helmet from 'helmet';
 
+import Resources from '../Resources/resources.js'
+
+const resources = new Resources()
+
 const menu = express()
 menu.use(helmet())
 
+const returnBack = resources.ReturnBackButton
+
+
 menu.get('/settings', (req,res) =>{
-    res.send('settings page')
+    menu.use(express.static('public/Menu'))
+    res.status(200).send(returnBack + resources.HtmlTemplates.Menu.Settings) 
     return
 })
 
