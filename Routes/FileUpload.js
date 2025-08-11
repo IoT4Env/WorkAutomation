732a6680270c228e8821348e9a21647eaf359e79
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 12:22:06 +0200

    Prepared method for uploading sql query.

--- Routes/FileUpload.js
+++ Routes/FileUpload.js
@@ -1,6 +1,18 @@
 import express from 'express'
 import storage from '../Resources/storage.js'
+import fs from 'fs'
 
 const fileUpload = express();
+
+
+fileUpload.post('/sql', storage.single('uploaded-file'), (req,res) =>{
+    const filePath = req.file.path
+    const query = fs.readFileSync(filePath, 'utf-8')
+    console.log(query);
+
+    fs.rmSync(filePath);
+    res.status(201).send('Data inserted successfully')
+    return;
+})
 
 export default fileUpload;
