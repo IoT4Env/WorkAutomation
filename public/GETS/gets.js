dfe082d62c7079a98e8f6a0a2e20196de55f8c56
IoT4Env <170166502+IoT4Env@users.noreply.github.com>
2025-08-11 12:24:34 +0200

    Styledish the confirm deletion window.

--- public/GETS/gets.js
+++ public/GETS/gets.js
@@ -2,55 +2,61 @@ const deletes = document.querySelectorAll('.delete-many'),
     deletesLink = document.querySelector('#delete-many-link'),
     deleteSelected = document.querySelector('#delete-selected'),
     deleteImgs = document.querySelectorAll('.delete-image'),
-    dialogBoxes = document.querySelectorAll('.confirm-delete-box')
+    dialogBoxes = document.querySelectorAll('.confirm-delete-box'),
+    cancelButton = document.querySelectorAll('.cancel')
 
+
+//#region Confirm deletion
+
+for (let i = 0; i < deleteImgs.length; i++) {
+    deleteImgs[i].addEventListener('click', _ => {
+        dialogBoxes[i].style.visibility = 'visible'
+        dialogBoxes[i].children.item(0).style.visibility = 'visible'
+        //item(0) because it refers to the div containing the confirm content
+    })
+    cancelButton[i].addEventListener('click', _ => {
+        dialogBoxes[i].style.visibility = 'hidden'
+        dialogBoxes[i].children.item(0).style.visibility = 'hidden'
+        //item(0) because it refers to the div containing the confirm content
+    })
+}
+//#endregion
 
 const linkString = deletesLink.getAttribute('href');
 let checkedArray = []
-
-let delImgIndex = 0
-
-//#region Confirm deletion
-deleteImgs.forEach(delImg =>{
-    delImg.addEventListener('click', _=>{
-        dialogBoxes[delImgIndex++].style.visibility = 'visible'
-    })
-})
-
-//#endregion
 
 //#region Delete many logic
 //deleteSelected button disabled by default
 deleteSelected.disabled = true
 
-deletes.forEach(del =>{
+deletes.forEach(del => {
     del.checked = false
-    del.addEventListener('click', _=>{
+    del.addEventListener('click', _ => {
         checkDelete(del)
     })
 })
 
-function checkDelete(del){
+function checkDelete(del) {
     let delId = del.getAttribute('id')
-    if(checkedArray.includes(delId))
+    if (checkedArray.includes(delId))
         checkedArray.splice(checkedArray.indexOf(delId), 1)
-    else{
+    else {
         checkedArray.push(delId)
     }
 
-    if(checkedArray.length > 0){
+    if (checkedArray.length > 0) {
         deleteSelected.disabled = false
-    }else{
+    } else {
         deleteSelected.disabled = true
     }
 }
 
 //#endregion
 
-deleteSelected.addEventListener('click', _=>{
-    
-    deletes.forEach(del =>{
-        if(del.checked)
+deleteSelected.addEventListener('click', _ => {
+
+    deletes.forEach(del => {
+        if (del.checked)
             checkedArray.push(del.getAttribute('id'))
     })
     const checkedJson = JSON.stringify(checkedArray)
